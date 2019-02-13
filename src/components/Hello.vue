b1acfe4acfc98504018257d5c5e6009caac553f9
svidlonix <sergey.svidlo@nixsolutions.com>
2019-02-13 19:01:47 +0200

    add redirect after login and flash messages valid/invalid request

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,15 +1,47 @@
 <template>
   <layout>
-    <p>Hello</p>
+    <flash-message></flash-message>
+    <div v-bind:key='advert' v-for='advert in adverts'>
+      <h3> {{advert}}</h3>
+    </div>
   </layout>
 </template>
 
 <script>
   import Layout from './layout/Layout.vue'
+  import axios from 'axios'
 
   export default {
     components: {
       layout: Layout
+    },
+    data () {
+      return {
+        adverts: []
+      }
+    },
+    async created () {
+      var result = await axios.get('http://localhost:3000//api/cars',
+        {
+          headers: {'Authorization': localStorage['user-token']},
+          params: {
+            limit: 10
+          },
+        }
+      )
+      .then(function (response) {
+        return response.data;
+      })
+      .catch(function (error) {
+        return error;
+      });
+
+      if (typeof result.message == 'undefined') {
+        this.adverts = result
+      } else {
+        this.flashError(result.message);
+      }
+
     }
   }
 </script>
